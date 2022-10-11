5695c14820922abe52de871ac6e0c8f9bd427051
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2022-10-10 22:46:34 -0400

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //global variables, can be accessed by all functions
 
   //declare a variable named playerName that stores the value the player enters from a prompt
-
+let playerName = prompt("Player, what is your name?")
   //declare a variable named playerHealth and set it equal to the number value 15
-
+const playerHealth = 15
   //assign a name of a monster (ex 'Werewolf') as a string to a variable named monsterName
-
+const monsterName = 'Gandolf'
   //declare a variable named monsterHealth and set it equal to the number value 15
-
+const monsterHealth = 15
 //random integer function 
 //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function randomNum(min, max) {
@@ -19,9 +19,13 @@ function randomNum(min, max) {
 
 function playerAttack(){
 //use randomNum to generate attack points value between 1 - 5 and save the value to a variable named playerAttackPoints
-
+let playerAttackPoints = randomNum(1,6)
 //subtract playerAttackPoints from monsterHealth and update the monsterHealth variable
 
+monsterHealth = monsterHealth - playerAttackPoints
+alert(`${playerName} has attacked ${monsterName}`)
+alert(`${playerName} has dealt ${playerAttackPoints} to ${monsterName}`)
+alert(`${monsterName} health is now ${monsterHealth}`)
   //use an alert with string template literals to tell the player: 
   // 1. player attacked monster 
   // 2. how much damage the player did 
@@ -30,9 +34,12 @@ function playerAttack(){
 
 function monsterAttack(){
   //use randomNum to generate attack points value between 1 - 5 and save the value to a variable named monsterAttackPoints
-
+let monsterAttackPoints = randomNum(1,6);
   //subtract monsterAttackPoints from playerHealth and update the playerHealth variable 
-
+playerHealth = playerHealth - monsterAttackPoints;
+alert(`${monsterName} has attacked ${playerName}`);
+alert(`${monsterName}has dealt ${monsterAttackPoints} to ${playerName}`);
+alert(`${playerName} health is now ${playerHealth}`);
   //use an alert with string template literals to tell the player: 
   // 1. monster attacked player 
   // 2. how much damage the monster did 
@@ -41,15 +48,24 @@ function monsterAttack(){
 
 function playRound() {
   //use randomNum to return either 0 or 1
-  
+  randomNum(0,2)
   //0 = player goes first, 1 = monster goes first
-  
+  playerTurn = randomNum(0,2)
   //use if/else to determine who goes first
   
   //if player goes first, run playerAttack, then if monsterHealth > 0, run monsterAttack
-
+  if (playerTurn === 0){
+    playerAttack();
+   if (monsterHealth > 0){
+    monsterAttack();}
+   }
+  else {
+    monsterAttack();
+    if (playerHealth > 0){
+      playerAttack()
+    }
+  }
   //if monster goes first, run monsterAttack, then if playerHealth > 0, run playerAttack 
-}
 
 function playGame() {
   //beginning game message
@@ -61,14 +77,21 @@ function playGame() {
 
   //while loop that runs until player or monster's health is <= 0 
   //add the condition in the while loop parentheses 
-  while(){
+  while(playerHealth >= 0 && monsterHealth >= 0){
     roundNumber++
    //write an alert statement that tells the player what round number it is, and the player's and monster's current health points
- 
+ alert(`It is now ${roundNumber}.${playerName} health is ${playerHealth} and ${monsterName} health is ${monsterHealth}.`)
    //call playRound inside the while loop
-    
+    playRound()
   }
   //outside of while loop, declare a winner and use alert to show a win or lose message 
-}
+  if (playerHealth >0 && monsterHealth <=0){
+    alert(`${playerName} You Win! | ${monsterName} You Lose!`);
+  }
+  else if(monsterHealth > 0 && playerHealth < 0);
+  {
+    alert(`${monsterName} You Win | ${playerName} You Lose!`);
+  }
 
 //call playGame to start game
+ playGame()
